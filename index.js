9f936209f1e6e635d36f33007f67276f5a4ca346
Rajeev Kumar Singh <rajeevkumar.web@gmail.com>
2016-12-04 07:18:49 +0530

    Calculator Method added

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    add(x, y) {
+        return x + y;
+    },
+
+    sub(x, y) {
+        return x - y;
+    },
+
+    mul(x, y) {
+        return x * y;
+    },
+
+    div(x, y) {
+        return x / y;
+    }
+};
